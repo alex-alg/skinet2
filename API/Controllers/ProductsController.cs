e86223fba1f57445ab9c4d47c185187865d46a52
AlexG <alex.alg.grecu@gmail.com>
2023-07-06 19:58:38 +0300

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            var products = _repo.GetProductsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+    }
+}
